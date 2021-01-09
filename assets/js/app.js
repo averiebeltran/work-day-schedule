437547bbbae5f43af29f52e1e051c773a6375559
AverieBeltran <averiebeltran@github.com>
2021-01-08 21:55:29 -0600

    displaying color for past hours

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,33 @@
 $(document).ready(function() {
     const dateTime = moment().format('MMMM Do YYYY, h:mm:ss a');
-    const dateTimeArr = dateTime.split(",");
-    const currentDate = dateTimeArr[0];
+    const dateTimeArr1 = dateTime.split(",");
+    const dateTimeArr2 = dateTime.split(" ");
+    const currentDate = dateTimeArr1[0];
+
+    const time = dateTimeArr1[1];
+    const amPM = dateTimeArr2[4];
+    const currentTimeArr = time.split(":");
+    const currentHour = currentTimeArr[0];
+    console.log(currentHour + amPM);
 
     $("#currentDay").html(currentDate);
 
-    const hours = ["9 a.m.", "10 a.m.", "11 a.m.", "12 p.m.","1 p.m.","2 p.m.","3 p.m.","4 p.m.","5 p.m."];
+    const hours = ["9am", "10am", "11am", "12pm","1pm","2pm","3pm","4pm","5pm"];
     jQuery.each(hours, function(index, val) {
-        console.log(index, val);
         const table = $(".container");
         table.append(`<div class="row">
-                        <div id="hour-` + index + `" class="hour">` + val + `</div>
+                        <div id="` + val + `" class="hour">` + val + `</div>
                         <textarea></textarea>
                         <button type="button" class="btn btn-secondary">Save <i class="fas fa-share-square"></i></button>
                     </div>`);
+
+        if ((currentHour + amPM) === $('#' + val)) {
+            console.log('the current hour');
+            $('#' + val).parent().addClass('present');
+        } else {
+            console.log('not the current hour');
+            $('#' + val).parent().addClass('past');
+        }
     });
 
 });
